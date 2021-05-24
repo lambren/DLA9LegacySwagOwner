ca3280cc6135e4b0a3c8297a719bf89060ce9b07
KuhnTran <KuhnTran@gmail.com>
2021-05-23 22:41:40 -0700

    Updates

---
+++ precache-manifest.07343371e85fde5d8faa574c0f1d50d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7140c03ce77fd212bcab38d672d8927c",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "ede483779ce5e03a23a9",
+    "url": "/react-gh-pages/static/css/main.8585c464.chunk.css"
+  },
+  {
+    "revision": "19beca18e0c7b0fa6593",
+    "url": "/react-gh-pages/static/js/2.a2e040c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.a2e040c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ede483779ce5e03a23a9",
+    "url": "/react-gh-pages/static/js/main.9aa3c4d2.chunk.js"
+  },
+  {
+    "revision": "1c189bb6a192246d6731",
+    "url": "/react-gh-pages/static/js/runtime-main.48948ee3.js"
+  }
+]);
