ceaac96517cb4e79a352980c7048915c5103ca37
KuhnTran <KuhnTran@gmail.com>
2021-05-23 22:47:17 -0700

    Updates

---
+++ precache-manifest.2d7762af68ca13cf8fb1846214c14e07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8e30e83892bc3480aa183e7ccd32014",
+    "url": "/DLA9LegacySwagOwner/index.html"
+  },
+  {
+    "revision": "9052ecc546fca218e180",
+    "url": "/DLA9LegacySwagOwner/static/css/main.8585c464.chunk.css"
+  },
+  {
+    "revision": "19beca18e0c7b0fa6593",
+    "url": "/DLA9LegacySwagOwner/static/js/2.a2e040c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DLA9LegacySwagOwner/static/js/2.a2e040c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9052ecc546fca218e180",
+    "url": "/DLA9LegacySwagOwner/static/js/main.a41190bc.chunk.js"
+  },
+  {
+    "revision": "4b5211d42012b1b33c4c",
+    "url": "/DLA9LegacySwagOwner/static/js/runtime-main.4d373f95.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.07343371e85fde5d8faa574c0f1d50d2.js"
+  "/DLA9LegacySwagOwner/precache-manifest.2d7762af68ca13cf8fb1846214c14e07.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/DLA9LegacySwagOwner/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
